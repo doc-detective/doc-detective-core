f8a5b3f7ec967ec8a58cc647e02d5b92db3faa1a
manuelrbsilva <48723033+manuelrbsilva@users.noreply.github.com>
2023-02-11 14:43:47 -0800

    Basic Appium server/driver config

--- test.js
+++ test.js
@@ -1,5 +1,42 @@
+const axios = require("axios");
+const appium = require("appium");
+const { exit } = require('node:process');
 const wdio = require('webdriverio');
+
+main();
+
+// Primary execution function.
 async function main() {
+    appiumStart();
+    await appiumIsReady();
+    await runTests();
+    exit();
+}
+
+// Start the Appium server asynchronously.
+async function appiumStart() {
+    appium.main();
+}
+
+// Delay execution until Appium server is available.
+async function appiumIsReady() {
+    let isReady = false;
+    while (!isReady) {
+        // Retry delay
+        // TODO: Add configurable retry delay
+        // TODO: Add configurable timeout duration
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        try {
+            let resp = await axios.get("http://localhost:4723/sessions");
+            if (resp.status === 200) isReady = true;
+        } catch { }
+    }
+    return isReady;
+}
+
+// Perform tests through Appium drivers.
+async function runTests() {
+    // Define driver capabilities.
     const caps = { "platformName": "windows", "appium:automationName": "Gecko", "browserName": "MozillaFirefox", "appium:newCommandTimeout": 3600, "appium:connectHardwareKeyboard": true }
     const driver = await wdio.remote({
         protocol: "http",
@@ -9,7 +46,19 @@ async function main() {
         capabilities: caps
     });
 
+    // Run through all browser-based actions.
+    // TODO: Go to URL
+    // TODO: Find element
+    // TODO: Match element text
+    // TODO: Click element
+    // TODO: Type keys
+    // TODO: Move mouse
+    // TODO: Scroll viewport
+    // TODO: Screenshot
+    // TODO: Start recording
+    // TODO: Stop recording
+    // TODO: Evaluate other Appium-supported actions
+
+    // End driver session.
     await driver.deleteSession();
 }
-
-main().catch(console.log);
