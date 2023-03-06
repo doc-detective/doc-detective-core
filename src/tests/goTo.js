2f847eecd3c179ee67a38a93c75b1e78a403c6b8
hawkeyexl <hawkeyexl@gmail.com>
2023-03-06 15:50:26 -0800

    Rewrote goTo action

--- src/tests/goTo.js
+++ src/tests/goTo.js
@@ -1,39 +1,42 @@
-const { setEnvs, loadEnvs } = require("../utils");
+const { loadEnvs } = require("../utils");
 const { validate } = require("doc-detective-common");
 
 exports.goTo = goTo;
 
 // Open a URI in the browser
-async function goTo(action, driver) {
+async function goTo(config, step, driver) {
   let result = { status: "", description: "" };
 
-  // Validate action payload
-  isValidAction = validate("goTo_v1", action);
-  if (!isValidAction.valid) {
-    result.status = "Invalid action.";
-    result.description = errors[0].message;
-    return { result };
+  // Load values from environment variables
+  step = loadEnvs(step);
+
+  // Validate step payload
+  isValidStep = validate("goTo_v2", step);
+  if (!isValidStep.valid) {
+    result.status = "FAIL";
+    result.description = `Invalid step definition: ${isValidStep.errors}`;
+    return result;
   }
 
-  // Load values from environment variables
-  action = loadEnvs(action);
+  // Make sure there's a protocol
+  if (!step.url.includes("://")) step.url = "https://" + step.url;
 
-  // Catch common formatting errors
-  if (!uri.includes("://")) uri = "https://" + uri;
+  // Set params
+  let url = step.url;
 
   // Run action
   try {
-    await driver.url(uri);
+    await driver.url(url);
   } catch {
-    // FAIL: Error opening URI
+    // FAIL: Error opening URL
     let status = "FAIL";
-    let description = "Couldn't open URI.";
+    let description = "Couldn't open URL.";
     let result = { status, description };
-    return { result };
+    return result;
   }
   // PASS
   let status = "PASS";
-  let description = "Opened URI.";
+  let description = "Opened URIL.";
   result = { status, description };
-  return { result };
+  return result;
 }
