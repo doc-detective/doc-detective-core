e78421d74a9d7519a56c6d90935c672f78df1bc1
hawkeyexl <hawkeyexl@gmail.com>
2023-03-13 15:42:37 -0700

    Added click to find step

--- src/tests/findElement.js
+++ src/tests/findElement.js
@@ -4,7 +4,7 @@ exports.findElement = findElement;
 
 // Find a single element
 async function findElement(config, step, driver) {
-  let result = { status: "", description: "" };
+  let result = { status: "PASS", description: "Found an element matching selector." };
 
   // Validate step payload
   isValidStep = validate("find_v2", step);
@@ -29,14 +29,32 @@ async function findElement(config, step, driver) {
   }
 
   // Match text
+  if (step.matchText) {
   const text = await element.getText();
   if (text !== step.matchText) {
     result.status = "FAIL";
     result.description = `Element text ("${text}") didn't equal match text ("${step.matchText}").`;
     return result;
   }
+    result.description = result.description + " Matched text.";
+  }
+
+
+  // Move mouse
+
+  // Click element
+  if (step.click) {
+    try {
+      await element.click();
+      result.description = result.description + " Clicked element.";
+    } catch {
+      // Couldn't click
+      result.status = "FAIL";
+      result.description = "Couldn't click element.";
+      return result;
+    }
+  }
+
   // PASS
-  result.status = "PASS";
-  result.description = "Found an element matching selector.";
   return result;
 }
