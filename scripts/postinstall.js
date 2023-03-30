47436755949001db74278d4495a84992f2d8ddcb
hawkeyexl <hawkeyexl@gmail.com>
2023-03-30 13:41:23 -0700

    Firefox and Chromium autoinstall

---
+++ scripts/postinstall.js
@@ -0,0 +1,15 @@
+main();
+
+async function main() {
+  const { BROWSERS } = await import("@eyeo/get-browser-binary");
+  // Install Chromium
+  console.log("Installing Chromium");
+  let chromium = await BROWSERS.chromium.installBrowser("latest");
+  // Install Firefox
+  console.log("Installing Firefox");
+  let firefox = await BROWSERS.firefox.installBrowser("latest");
+  // TODO: Installing Edge requires superuser privileges on Linux
+  // console.log("Installing Edge");
+  // let edge = await BROWSERS.edge.installBrowser("latest");
+  // TODO: Catch misc install errors
+}
