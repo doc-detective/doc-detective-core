dfba199977bccaca6ec1eb25e7de11f0867a7fac
hawkeyexl <hawkeyexl@gmail.com>
2023-10-09 16:00:26 -0700

    Added setCookies action

---
+++ src/tests/setCookies.js
@@ -0,0 +1,40 @@
+const { validate } = require("doc-detective-common");
+
+exports.setCookies = setCookies;
+
+// Open a URI in the browser
+async function setCookies(config, step, driver) {
+  let result = { status: "PASS", description: "Set cookies." };
+
+  // Check if cookies are stringified JSON
+  if (step.cookies && typeof step.cookies === "string") {
+    try {
+      step.cookies = JSON.parse(step.cookies);
+    } catch {
+      result.status = "FAIL";
+      result.description = "Couldn't parse cookies.";
+      return result;
+    }
+  }
+
+  // Validate step payload
+  isValidStep = validate("setCookies_v2", step);
+  if (!isValidStep.valid) {
+    result.status = "FAIL";
+    result.description = `Invalid step definition: ${isValidStep.errors}`;
+    return result;
+  }
+
+  // Run action
+  try {
+    await driver.setCookies(step.cookies);
+  } catch {
+    // FAIL: Error getting cookies
+    result.status = "FAIL";
+    result.description = "Couldn't get cookies.";
+    return result;
+  }
+  
+  // PASS
+  return result;
+}
