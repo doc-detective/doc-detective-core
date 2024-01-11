96efe8b5c345cde5df1b65345540b797b984e10b
hawkeyexl <hawkeyexl@gmail.com>
2024-01-11 15:07:37 -0800

    Refactor stopRecording function and add file conversion

--- src/tests/stopRecording.js
+++ src/tests/stopRecording.js
@@ -1,8 +1,10 @@
 const { validate } = require("doc-detective-common");
+const { log } = require("../utils");
 const path = require("path");
 const fs = require("fs");
-const { spawn } = require("child_process");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+const ffmpeg = require("fluent-ffmpeg");
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 exports.stopRecording = stopRecording;
 
@@ -22,8 +24,8 @@ async function stopRecording(config, step, driver) {
 
   // Skip if recording is not started
   if (!config.recording) {
-    result.status = "SKIP";
-    result.description = `Recording is not started.`;
+    result.status = "SKIPPED";
+    result.description = `Recording isn't started.`;
     return result;
   }
 
@@ -31,8 +33,6 @@ async function stopRecording(config, step, driver) {
     if (config.recording.type === "MediaRecorder") {
       // MediaRecorder
 
-      // Get current tab
-      const currentTab = await driver.getWindowHandle();
       // Switch to recording tab
       await driver.switchToWindow(config.recording.tab);
       // Stop recording
@@ -40,47 +40,37 @@ async function stopRecording(config, step, driver) {
         window.recorder.stop();
       });
       // Wait for file to be in download path
-      await driver.waitUntil(async () => {
-        return fs.existsSync(config.recording.downloadPath);
-      });
+      while (!fs.existsSync(config.recording.downloadPath)) {
+        await new Promise((r) => setTimeout(r, 1000));
+      }
       // Close recording tab
       await driver.closeWindow();
-      await driver.switchToWindow(currentTab);
 
-      // Identify if the file needs to be converted. If so, convert it.
-      if (
-        path.extname(config.recording.downloadPath) !==
-        path.extname(config.recording.targetPath)
-      ) {
-        // TODO: Set video format options
-        // let vf = "";
-        // if (path.extname(config.recording.targetPath) === ".gif") {
-        //   vf = "fps=30,scale=-1:-1:flags=lanczos";
-        // }
-        // Convert file
-        const ffmpeg = spawn(ffmpegPath, [
-          "-y",
-          "-i",
-          config.recording.downloadPath,
-          "-max_muxing_queue_size 9999",
-          config.recording.targetPath,
-        ]);
-      } else {
-        // If file already exists, delete it
-        if (fs.existsSync(config.recording.targetPath)) {
-          await fs.promises.unlink(config.recording.targetPath);
-        }
-        // Move file from download path to target path
-        await fs.promises.rename(
-          config.recording.downloadPath,
-          config.recording.targetPath
-        );
-      }
+      // Convert the file into the target format/location
+      const endMessage = `Finished processing file: ${config.recording.targetPath}`;
+      const downloadPath = `${config.recording.downloadPath}`;
+      ffmpeg(`${config.recording.downloadPath}`)
+        .output(`${config.recording.targetPath}`)
+        .on("end", () => {
+          // Delete the downloaded file
+          fs.unlinkSync(downloadPath);
+          log(config, 'debug', endMessage);
+        })
+        .on("error", (err) => {
+          log(config, 'error', err);
+        })
+        // TODO: Specify output options based on file type
+        // .outputOptions(() => {
+        //   if (path.extname(config.recording.targetPath) === ".gif") {
+        //     return ["-vf", "scale=320:-1:flags=lanczos"];
+        //   } else {
+        //     return [];
+        //   }
+        // })
+        .run();
     } else {
       // FFMPEG
-
-      config.recording.stdin.write("q");
-      // await driver.startRecording(step.filePath);
+      // config.recording.stdin.write("q");
     }
   } catch (error) {
     // Couldn't stop recording
