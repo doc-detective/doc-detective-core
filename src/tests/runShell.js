2a65d013cc84c372a9b27fbd48704400301d8ca1
hawkeyexl <hawkeyexl@gmail.com>
2024-03-17 12:19:14 -0700

    Added exitCode and output evaluation

--- src/tests/runShell.js
+++ src/tests/runShell.js
@@ -6,16 +6,16 @@ exports.runShell = runShell;
 // Run a shell command.
 async function runShell(config, step) {
   // Promisify and execute command
-  let result = {
-    status: "",
-    description: "",
+  const result = {
+    status: "PASS",
+    description: "Executed command.",
     exitCode: "",
     stdout: "",
     stderr: "",
   };
 
   // Validate step object
-  isValidStep = validate("runShell_v2", step);
+  const isValidStep = validate("runShell_v2", step);
   if (!isValidStep.valid) {
     result.status = "FAIL";
     result.description = `Invalid step definition: ${isValidStep.errors}`;
@@ -24,18 +24,35 @@ async function runShell(config, step) {
 
   // Execute command
   const commandResult = await spawnCommand(step.command, step.args);
-  result.stdout = commandResult.stdout;
-  result.stderr = commandResult.stderr;
+  result.stdout = commandResult.stdout.replace(/\r$/, "");
+  result.stderr = commandResult.stderr.replace(/\r$/, "");
   result.exitCode = commandResult.exitCode;
 
-  // Evaluate result
-  if (result.exitCode) {
+  // Evaluate exit code
+  if (!step.exitCodes.includes(result.exitCode)) {
     result.status = "FAIL";
-    result.description = `Error during execution.`;
-  } else {
-    result.status = "PASS";
-    result.description = `Executed command.`;
+    result.description = `Returned exit code ${
+      result.exitCode
+    }. Expected one of ${JSON.stringify(step.exitCodes)}`;
   }
+
+  // Evaluate stdout and stderr
+  // If step.output starts and ends with `/`, treat it as a regex
+  if (step.output.startsWith("/") && step.output.endsWith("/")) {
+    const regex = new RegExp(step.output.slice(1, -1));
+    if (!regex.test(result.stdout) && !regex.test(result.stderr)) {
+      result.status = "FAIL";
+      result.description = `Couldn't find expected output (${step.output}) in actual output (stdout or stderr).`;
+    }
+  } else {
+    if (!result.stdout.includes(step.output) && !result.stderr.includes(step.output)) {
+      result.status = "FAIL";
+      result.description = `Couldn't find expected output (${step.output}) in actual output (stdout or stderr).`;
+    }
+  }
+
+  console.log(result)
+  process.exit()
 
   return result;
 }
