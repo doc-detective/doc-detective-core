19c72d47f3ac60d185a25b54cd9379f3fb21529a
hawkeyexl <hawkeyexl@gmail.com>
2024-06-29 15:45:53 -0700

    Added output saving and variation detection to runShell

--- src/tests/runShell.js
+++ src/tests/runShell.js
@@ -1,5 +1,7 @@
 const { validate } = require("doc-detective-common");
-const { spawnCommand } = require("../utils");
+const { spawnCommand, log } = require("../utils");
+const fs = require("fs");
+const path = require("path");
 
 exports.runShell = runShell;
 
@@ -88,5 +90,94 @@ async function runShell(config, step) {
     }
   }
 
+  // Check if command output is saved to a file
+  if (step.savePath) {
+    const dir =
+      step.directory ||
+      config.runTests?.mediaDirectory ||
+      config.runTests?.output ||
+      config.output;
+    // If `dir` doesn't exist, create it
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    // Set filePath
+    const filePath = path.resolve(dir, step.savePath);
+    log(config,"debug", `Saving output to file: ${filePath}`)
+
+    // Check if file already exists
+    if (!fs.existsSync(filePath)) {
+      // Doesn't exist, save output to file
+      fs.writeFileSync(filePath, result.stdout)
+    } else {
+      if (step.overwrite == "false") {
+        // File already exists
+        result.description =
+          result.description + ` Didn't save output. File already exists.`;
+      }
+
+      // Read existing file
+      const existingFile = fs.readFileSync(filePath, "utf8");
+
+      // Calculate percentage diff between existing file content and command output content, not length
+      const percentDiff = calculatePercentageDifference(
+        existingFile,
+        result.stdout
+      );
+      log(config,"debug", `Percentage difference: ${percentDiff}%`);
+
+      if (percentDiff > step.maxVariation) {
+        if (step.overwrite == "byVariation") {
+          // Overwrite file
+          fs.writeFileSync(filePath, result.stdout);
+        }
+        result.status = "FAIL";
+        result.description =
+          result.description +
+          ` The percentage difference between the existing file content and command output content (${percentDiff}%) is greater than the max accepted variation (${step.maxVariation}%).`;
+        return result;
+      }
+
+      if (step.overwrite == "true") {
+        // Overwrite file
+        fs.writeFileSync(filePath, result.stdout);
+      }
+    }
+  }
+
   return result;
+}
+
+function calculatePercentageDifference(text1, text2) {
+  const distance = llevenshteinDistance(text1, text2);
+  const maxLength = Math.max(text1.length, text2.length);
+  const percentageDiff = (distance / maxLength) * 100;
+  return percentageDiff.toFixed(2); // Returns the percentage difference as a string with two decimal places
+}
+
+function llevenshteinDistance(s, t) {
+  if (!s.length) return t.length;
+  if (!t.length) return s.length;
+
+  const arr = [];
+
+  for (let i = 0; i <= t.length; i++) {
+    arr[i] = [i];
+  }
+
+  for (let j = 0; j <= s.length; j++) {
+    arr[0][j] = j;
+  }
+
+  for (let i = 1; i <= t.length; i++) {
+    for (let j = 1; j <= s.length; j++) {
+      arr[i][j] = Math.min(
+        arr[i - 1][j] + 1, // deletion
+        arr[i][j - 1] + 1, // insertion
+        arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1) // substitution
+      );
+    }
+  }
+
+  return arr[t.length][s.length];
 }
