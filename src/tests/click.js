b4c43c9d397798abcccf6ba8da13557c926e7347
hawkeyexl <hawkeyexl@gmail.com>
2025-04-04 15:53:52 -0700

    First pass at click step

--- src/tests/click.js
+++ src/tests/click.js
@@ -1,22 +1,63 @@
+const { findElement } = require("./findElement");
+
 exports.clickElement = clickElement;
 
 // Click an element.
-async function clickElement(element) {
-    let status;
-    let description;
-    let result;
-    try {
-        await element.click();
-    } catch {
-        // FAIL: Text didn't match
-        status = "FAIL";
-        description = `Couldn't click element.`;
-        result = { status, description };
-        return { result };
+async function clickElement({ config, step, driver, element }) {
+  const result = {
+    status: "PASS",
+    description: "Clicked element.",
+  };
+
+  // Validate step payload
+  const isValidStep = validate({ schemaKey: "step_v3", object: step });
+  if (!isValidStep.valid) {
+    result.status = "FAIL";
+    result.description = `Invalid step definition: ${isValidStep.errors}`;
+    return result;
+  }
+  // Accept coerced and defaulted values
+  step = isValidStep.object;
+  // Set default values
+  step.click = {
+    ...step.click,
+    button: step.click.button || "left",
+  };
+
+  if (!element?.elementId) {
+    const findStep = {
+      find: {
+        selector: step.click.selector,
+        elementText: step.click.elementText,
+      },
+    };
+    // Find element
+    const findResult = await findElement({
+      config,
+      step: findStep,
+      driver,
+    });
+    if (findResult.status === "FAIL") {
+      return findResult;
     }
-    // PASS
-    status = "PASS";
-    description = `Clicked element.`;
-    result = { status, description };
-    return { result };
+    element = findResult.outputs.element;
+    if (!element) {
+      result.status = "FAIL";
+      result.description = `Couldn't find element.`;
+      return result;
+    }
+  }
+
+  try {
+    await element.click({
+      button: step.click.button,
+    });
+    result.description += " Clicked element.";
+  } catch (error) {
+    result.status = "FAIL";
+    result.description = `Couldn't click element. Error: ${error.message}`;
+    return result;
+  }
+  // PASS
+  return result;
 }
