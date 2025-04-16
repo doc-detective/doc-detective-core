177f81025c0214c6a92f2dc487f070172f0d6e81
hawkeyexl <hawkeyexl@gmail.com>
2025-04-16 13:50:14 -0700

    Removed runCoverage and suggestTest operations

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const { setConfig } = require("./config");
 const { qualityFiles, parseTests, log, cleanTemp } = require("./utils");
 const { runSpecs } = require("./tests");
-const { checkTestCoverage, checkMarkupCoverage } = require("./analysis");
-const { getSuggestions } = require("./suggest");
 const { telemetryNotice, sendTelemetry } = require("./telem");
 
 exports.runTests = runTests;
-exports.runCoverage = runCoverage;
-exports.suggestTests = suggestTests;
 
 const supportMessage = `
 ##########################################################################
@@ -51,47 +47,4 @@ async function runTests(config) {
   log(config, "info", supportMessage);
 
   return results;
-}
-
-// Calculate test coverage of doc content.
-async function runCoverage(config) {
-  // Set config
-  config = await setConfig(config);
-  log(config, "debug", `CONFIG:`);
-  log(config, "debug", config);
-
-  // Telemetry notice
-  telemetryNotice(config);
-
-  // Set files
-  const files = await qualityFiles(config);
-  log(config, "debug", `FILES:`);
-  log(config, "debug", files);
-
-  const testCoverage = checkTestCoverage(config, files);
-  log(config, "debug", "TEST COVERAGE:");
-  log(config, "debug", testCoverage);
-
-  const markupCoverage = checkMarkupCoverage(config, testCoverage);
-  log(config, "debug", "MARKUP COVERAGE:");
-  log(config, "debug", markupCoverage);
-
-  // Send telemetry
-  sendTelemetry(config, "runCoverage", markupCoverage);
-  log(config, "info", supportMessage);
-
-  return markupCoverage;
-}
-
-async function suggestTests(config) {
-
-  const markupCoverage = await runCoverage(config);
-  log(config, "debug", "MARKUP COVERAGE:");
-  log(config, "debug", markupCoverage);
-
-  const suggestionReport = getSuggestions(config, markupCoverage);
-  log(config, "debug", "TEST SUGGESTIONS:");
-  log(config, "debug", suggestionReport);
-
-  return suggestionReport;
 }
